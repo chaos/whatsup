e25d18d0a113411d64fb8b7034788520b22c04c9
Albert Chu <chu11@llnl.gov>
2003-08-18 22:22:08 +0000

    clean up test

--- src/Nodeupdown/test.pl
+++ src/Nodeupdown/test.pl
@@ -22,21 +22,8 @@ foreach $i (@list) {
 }
 print "\n\n";
 
-$obj->up_nodes();
-$obj->down_nodes();
-
 print $obj->are_up("devi"),"\n";
-print $obj->are_up("edevi"),"\n";
-print $obj->are_up("foodevi"),"\n";
 print $obj->are_up("devi","dev0"),"\n";
-print $obj->are_up("edevi","dev0"),"\n";
-print $obj->are_up("foodevi","dev0"),"\n";
-print $obj->are_up(),"\n";
 
 print $obj->are_down("devi"),"\n";
 print $obj->are_down("edevi"),"\n";
-print $obj->are_down("foodevi"),"\n";
-print $obj->are_down("devi","dev0"),"\n";
-print $obj->are_down("edevi","dev0"),"\n";
-print $obj->are_down("foodevi","dev0"),"\n";
-print $obj->are_down(),"\n";
