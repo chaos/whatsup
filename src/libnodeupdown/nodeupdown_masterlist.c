5b732950def26312ec37141530ee5f7e0c5204cd
Albert Chu <chu11@llnl.gov>
2003-12-29 19:02:02 +0000

    Add copyright and DISCLAIMER

--- src/libnodeupdown/nodeupdown_masterlist.c
+++ src/libnodeupdown/nodeupdown_masterlist.c
@@ -1,8 +1,28 @@
-/*
- *  $Id: nodeupdown_masterlist.c,v 1.8 2003-11-24 16:34:53 achu Exp $
- *  $Source: /g/g0/achu/temp/whatsup-cvsbackup/whatsup/src/libnodeupdown/nodeupdown_masterlist.c,v $
- *    
- */
+/*****************************************************************************\
+ *  $Id: nodeupdown_masterlist.c,v 1.9 2003-12-29 19:02:02 achu Exp $
+ *****************************************************************************
+ *  Copyright (C) 2003 The Regents of the University of California.
+ *  Produced at Lawrence Livermore National Laboratory (cf, DISCLAIMER).
+ *  Written by Albert Chu <chu11@llnl.gov>
+ *  UCRL-CODE-155699
+ *  
+ *  This file is part of Whatsup, tools and libraries for determining up and
+ *  down nodes in a cluster. For details, see http://www.llnl.gov/linux/.
+ *  
+ *  Whatsup is free software; you can redistribute it and/or modify 
+ *  it under the terms of the GNU General Public License as published by the 
+ *  Free Software Foundation; either version 2 of the License, or (at your 
+ *  option) any later version.
+ *  
+ *  Whatsup is distributed in the hope that it will be useful, but 
+ *  WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY 
+ *  or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License 
+ *  for more details.
+ *  
+ *  You should have received a copy of the GNU General Public License along
+ *  with Whatsup; if not, write to the Free Software Foundation, Inc.,
+ *  59 Temple Place, Suite 330, Boston, MA  02111-1307  USA.
+\*****************************************************************************/
 
 #if HAVE_CONFIG_H
 #include "config.h"
