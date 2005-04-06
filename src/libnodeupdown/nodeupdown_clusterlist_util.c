29bfe66c9762b65c050068fd56810bffc129876f
Albert Chu <chu11@llnl.gov>
2005-04-06 03:46:10 +0000

    new files

---
+++ src/libnodeupdown/nodeupdown_clusterlist_util.c
@@ -0,0 +1,57 @@
+/*****************************************************************************\
+ *  $Id: nodeupdown_clusterlist_util.c,v 1.1 2005-04-06 03:46:10 achu Exp $
+ *****************************************************************************
+ *  Copyright (C) 2003 The Regents of the University of California.
+ *  Produced at Lawrence Livermore National Laboratory (cf, DISCLAIMER).
+ *  Written by Albert Chu <chu11@llnl.gov>
+ *  UCRL-CODE-155699
+ *  
+ *  This file is part of Whatsup, tools and libraries for determining up and
+ *  down nodes in a cluster. For details, see http://www.llnl.gov/linux/.
+ *  
+ *  Whatsup is free software; you can redistribute it and/or modify 
+ *  it under the terms of the GNU General Public License as published by the 
+ *  Free Software Foundation; either version 2 of the License, or (at your 
+ *  option) any later version.
+ *  
+ *  Whatsup is distributed in the hope that it will be useful, but 
+ *  WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY 
+ *  or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License 
+ *  for more details.
+ *  
+ *  You should have received a copy of the GNU General Public License along
+ *  with Whatsup; if not, write to the Free Software Foundation, Inc.,
+ *  59 Temple Place, Suite 330, Boston, MA  02111-1307  USA.
+\*****************************************************************************/
+
+#if HAVE_CONFIG_H
+#include "config.h"
+#endif /* HAVE_CONFIG_H */
+ 
+#include <stdio.h>
+#include <stdlib.h>
+#if STDC_HEADERS
+#include <string.h>
+#endif /* STDC_HEADERS */
+
+#include "nodeupdown.h"
+#include "nodeupdown_common.h"
+#include "nodeupdown_clusterlist_util.h"
+
+int 
+nodeupdown_clusterlist_copy_nodename(nodeupdown_t handle, const char *node, char *buf, unsigned int buflen)
+{
+  int len;
+ 
+  len = strlen(node);
+ 
+  if ((len + 1) > buflen)
+    {
+      handle->errnum = NODEUPDOWN_ERR_INTERNAL;
+      return -1;
+    }
+ 
+  strcpy(buf, node);
+ 
+  return 0;
+}
