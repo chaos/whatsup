fdcb55593e5d45bf24e5ca09861eb723a7cae810
Albert Chu <chu11@llnl.gov>
2005-05-07 18:06:53 +0000

    new files

---
+++ src/libnodeupdown/nodeupdown_backend_util.h
@@ -0,0 +1,45 @@
+/*****************************************************************************\
+ *  $Id: nodeupdown_backend_util.h,v 1.1 2005-05-07 18:06:53 achu Exp $
+ *****************************************************************************
+ *  Copyright (C) 2003 The Regents of the University of California.
+ *  Produced at Lawrence Livermore National Laboratory (cf, DISCLAIMER).
+ *  Written by Albert Chu <chu11@llnl.gov>
+ *  UCRL-CODE-155699
+ *  
+ *  This file is part of Whatsup, tools and libraries for determining up and
+ *  down nodes in a cluster. For details, see http://www.llnl.gov/linux/.
+ *  
+ *  Whatsup is free software; you can redistribute it and/or modify 
+ *  it under the terms of the GNU General Public License as published by the 
+ *  Free Software Foundation; either version 2 of the License, or (at your 
+ *  option) any later version.
+ *  
+ *  Whatsup is distributed in the hope that it will be useful, but 
+ *  WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY 
+ *  or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License 
+ *  for more details.
+ *  
+ *  You should have received a copy of the GNU General Public License along
+ *  with Whatsup; if not, write to the Free Software Foundation, Inc.,
+ *  59 Temple Place, Suite 330, Boston, MA  02111-1307  USA.
+\*****************************************************************************/
+
+#ifndef _NODEUPDOWN_UTIL_H
+#define _NODEUPDOWN_UTIL_H
+
+#include "nodeupdown.h"
+
+/* 
+ * _nodeupdown_util_low_timeout_connect
+ *
+ * Setup a tcp connection to 'hostname' and 'port' using a connection
+ * timeout of 'connect_timeout'.
+ *
+ * Return file descriptor on success, -1 on error.
+ */
+int _nodeupdown_util_low_timeout_connect(nodeupdown_t handle, 
+					 const char *hostname, 
+					 int port,
+					 int connect_timeout); 
+
+#endif /* _NODEUPDOWN_UTIL_H */
