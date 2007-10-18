ff8a99c884333f6466d768efdbd7c49f81a5e84c
Albert Chu <chu11@llnl.gov>
2007-10-18 16:12:18 +0000

    revert copyright, b/c nothingwas changed in this file

--- src/libcommon/debug.c
+++ src/libcommon/debug.c
@@ -1,30 +1,5 @@
 /*****************************************************************************\
- *  $Id: debug.c,v 1.4 2007-10-17 21:49:34 chu11 Exp $
- *****************************************************************************
- *  Copyright (C) 2007 Lawrence Livermore National Security, LLC.
- *  Copyright (C) 2003-2007 The Regents of the University of California.
- *  Produced at Lawrence Livermore National Laboratory (cf, DISCLAIMER).
- *  Written by Albert Chu <chu11@llnl.gov>
- *  UCRL-CODE-155699
- *
- *  This file is part of Whatsup, tools and libraries for determining up and
- *  down nodes in a cluster. For details, see http://www.llnl.gov/linux/.
- *
- *  The code in this file began with the code in the Cerebro project.
- *  See below for original copyright information.
- *
- *  Whatsup is free software; you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by the
- *  Free Software Foundation; either version 2 of the License, or (at your
- *  option) any later version.
- *
- *  Whatsup is distributed in the hope that it will be useful, but
- *  WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
- *  or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
- *  for more details.
- *
- *  You should have received a copy of the GNU General Public License along
- *  with Whatsup.  If not, see <http://www.gnu.org/licenses/>.
+ *  $Id: debug.c,v 1.5 2007-10-18 16:12:18 chu11 Exp $
  *****************************************************************************
  *  Copyright (C) 2005 The Regents of the University of California.
  *  Produced at Lawrence Livermore National Laboratory (cf, DISCLAIMER).
