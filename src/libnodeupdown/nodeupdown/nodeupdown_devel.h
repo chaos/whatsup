77ce8ed68b462c6d216990f4d096ff4ec517c3ad
Albert Chu <chu11@llnl.gov>
2008-03-28 17:06:39 +0000

    update copyright in a lot of places

--- src/libnodeupdown/nodeupdown/nodeupdown_devel.h
+++ src/libnodeupdown/nodeupdown/nodeupdown_devel.h
@@ -1,7 +1,7 @@
 /*****************************************************************************\
- *  $Id: nodeupdown_devel.h,v 1.5 2007-10-16 23:55:23 chu11 Exp $
+ *  $Id: nodeupdown_devel.h,v 1.6 2008-03-28 17:06:38 chu11 Exp $
  *****************************************************************************
- *  Copyright (C) 2007 Lawrence Livermore National Security, LLC.
+ *  Copyright (C) 2007-2008 Lawrence Livermore National Security, LLC.
  *  Copyright (C) 2003-2007 The Regents of the University of California.
  *  Produced at Lawrence Livermore National Laboratory (cf, DISCLAIMER).
  *  Written by Albert Chu <chu11@llnl.gov>
